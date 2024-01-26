2d9f8436a7cd09817fda7c5f3bcb9d60d2352704
pasDamola <oyindadavid18@gmail.com>
2024-01-26 02:47:47 +0100

    feat: implemented the controllers, services, routes and tests for the air quality feature

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.ts", "!src/__tests__/**/*.ts"],
 };
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log("Example app listening on port 3000!"));
