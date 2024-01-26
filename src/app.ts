6055e67de9dbfa33b842447505922ae5f03d9a9f
pasDamola <oyindadavid18@gmail.com>
2024-01-26 08:48:53 +0100

    chore: fix build script for CI/CD

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(logger);
 
+console.log(`Hello ${process.env.IQAIR_API_KEY}`);
+
 // Routes
 app.use("/api", airQualityRoutes);
 
