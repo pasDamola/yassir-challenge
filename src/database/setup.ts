89279990579dd7bd705b594f50962e94d00fd91c
pasDamola <oyindadavid18@gmail.com>
2024-01-29 08:12:11 +0100

    fix: fixed databse setup for mongo connection

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI || "");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
