1deb93bc7c502010add30d427e7da1a972f55c71
pasDamola <oyindadavid18@gmail.com>
2024-01-29 08:18:26 +0100

    fix: fixed error for mongodb connection

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "");
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/airquality"
+    );
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
